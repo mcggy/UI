608767eecf493c7aedf9ac574fe7cc6d8f12a020
Administrator <1445866338@qq.com>
2016-10-12 16:30:53 +0800

    add acticity

---
+++ app/src/main/java/activity/PreferenceActivityTest.java
@@ -0,0 +1,22 @@
+package activity;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListAdapter;
+
+import com.example.administrator.ui.R;
+
+/**
+ * Created by Administrator on 2016/10/12.
+ */
+
+public class PreferenceActivityTest extends PreferenceActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
